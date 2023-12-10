6ea8914e9a83526bf11aaf0a314d8c8af3eb6e38
rumman99 <tasnimrumman369@gmail.com>
2023-12-10 22:17:42 +0600

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import './App.css'
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import firebaseConfig from './firebase_config';
+import { useState } from 'react';
 
 
 const app = initializeApp(firebaseConfig);
 
 function App() {
   const provider = new GoogleAuthProvider();
-  const handleButton=()=>{
+
+  const [user, setUser]=useState({isLogin:false, name:'', email: '', photo:''});
+  const handleLogin=()=>{
     const auth = getAuth();
     signInWithPopup(auth, provider)
     .then(res=>{
       const {displayName, email, photoURL}= res.user;
-      console.log(displayName, email, photoURL);
+      setUser({isLogin: true, name: displayName, email:email, photo:photoURL})
     })
-    
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+  const handleLogout=()=>{
+    const auth = getAuth();
+    signOut(auth)
+    .then(()=>{
+    setUser({isLogin:false, name:'', email: '', photo:''})
+    })
+    .catch(err=>{
+      console.log(err);
+    }) 
   }
 
   return (
     <>
-      <button onClick={handleButton}>Sign In</button>
-      {/* <h3>You Are {email}</h3> */}
+    {user.isLogin ? <button onClick={handleLogout}>Sign Out</button> : <button onClick={handleLogin}>Sign In</button>}
+      
+      {user.isLogin && 
+      <div>
+        <h3>Hello {user.name}</h3>
+        <h3>Your Mail {user.email}</h3>
+        <img src={user.photo} alt="" />
+      </div>}
     </>
   )
 }
